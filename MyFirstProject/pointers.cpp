6ae04dbc101367307b4bc40d58c48e9fa032bbf1
TahaSoylu44 <thsoylu44@gmail.com>
2025-10-09 18:52:53 +0300

    pointers reviewed

--- MyFirstProject/pointers.cpp
+++ MyFirstProject/pointers.cpp
@@ -1,13 +1,51 @@
 #include <iostream>
 
+using namespace std;
+
+void square(int n){
+    n = n * n;
+}
+
 int main(){
+    // string* pName = nullptr;
+    // string name = "Taha";
 
-    int mynum = 44;
+    // pName = &name;
 
-    int* pMynum = nullptr;
+    // string** ppName = &pName;   //Do not forget to specify the dimension.
 
-    pMynum = &mynum;
+    // cout << "The adress of the pointer of the name: " << ppName << endl;
+    // cout << "The adress of the name: " << pName << endl;
+    // cout << "The name itself: " << *pName << endl;
 
-    std::cout << &mynum << std::endl;
-    std::cout << pMynum << std::endl;
+    // cout << "################" << endl;
+
+    // int arr[3] = {1, 2, 3};
+    // int* pArr = arr;
+
+    // cout << "The address of the array: "<< pArr << endl;
+    // pArr++; //go to the next element
+    // cout << "The address of the array: "<< pArr << endl;
+
+    //Dynamic Memory
+    // int* pNum = new int;
+    // *pNum = 44;
+    
+    // cout << "The address of the num: "<< pNum << endl;
+    // cout << "The num itself:  "<< *pNum << endl;
+
+    // delete pNum;
+
+    // return 0;
+
+    int* pNum = new int;
+    *pNum = 44;
+    square(*pNum);
+
+    cout << *pNum << endl;
+
+    return 0;
+
+
+
 }
